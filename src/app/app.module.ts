16814ce39af2d948843e12c71c60e372dc775281
Maksym-Paselsky <43058251+Maksym-Paselsky@users.noreply.github.com>
2018-12-18 18:46:38 +0200

    add poster

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PosterComponent } from './custom-components/poster/poster.component';
+import { FormsModule } from '@angular/forms';
+import { CustomComponentModule } from './custom-components/customComponents.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CustomComponentModule,
+    FormsModule,
+    BrowserModule,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
