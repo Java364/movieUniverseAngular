9936e152d1c877af812b44c2972a0fb98b788bbb
Maksym-Paselsky <43058251+Maksym-Paselsky@users.noreply.github.com>
2018-12-25 13:26:27 +0200

    add service

--- src/app/custom-component/user-review-mark/UserReviewMark.service.ts
+++ src/app/custom-component/user-review-mark/UserReviewMark.service.ts
@@ -1,1 +1,43 @@
-user
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserReviewMark } from './userReviewMark';
+@Injectable()
+export class UserReviewMarkService {
+    constructor(private httpClient: HttpClient) {
+    }
+    public getUserReviewMark(id: number, callback: Function) {
+        this.httpClient.get('http://localhost:8080/userReviewMark/' + id).subscribe(
+            (success) => {
+                callback(success);
+            }
+        );
+    }
+    public createUserReviewMark(userReviewMark: UserReviewMark, callback: Function) {
+        this.httpClient.post('http://localhost:8080/userReviewMark', userReviewMark).subscribe(
+            (success) => {
+                callback(success);
+            }
+        );
+    }
+    public updateUserReviewMark(id: number, userReviewMark: UserReviewMark, callback: Function, ) {
+        this.httpClient.put('http://localhost:8080/userReviewMark/' + id, userReviewMark).subscribe(
+            (success) => {
+                callback(success);
+            }
+        );
+    }
+    public deleteUserReviewMark(id: number, callback: Function) {
+        this.httpClient.delete('http://localhost:8080/userReviewMark/' + id).subscribe(
+            (success) => {
+                callback(success);
+            }
+        );
+    }
+    public getAll(callback: Function) {
+        this.httpClient.get('http://localhost:8080/userReviewMarks').subscribe(
+            (success) => {
+                callback(success);
+            }
+        );
+    }
+}
