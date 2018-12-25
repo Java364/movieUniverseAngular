e6eabefe1a317b025e61d729ff39e2b965803d8a
Maksym-Paselsky <43058251+Maksym-Paselsky@users.noreply.github.com>
2018-12-25 13:23:20 +0200

    d

--- src/app/custom-component/user-review-mark/user-review-mark.component.ts
+++ src/app/custom-component/user-review-mark/user-review-mark.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UserReviewMark } from './userReviewMark';
+import { UserReviewMarkService } from './userReviewMark.service';
 
 @Component({
   selector: 'app-user-review-mark',
@@ -6,10 +8,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-review-mark.component.css']
 })
 export class UserReviewMarkComponent implements OnInit {
+  public userReviewMark: UserReviewMark;
+  public userReviewMarkList: UserReviewMark[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private userReviewMarkService: UserReviewMarkService) {
+    this.userReviewMark = new UserReviewMark();
   }
 
+  ngOnInit() : void{
+    this.testAll();
+  }
+  testGet = (id: number) => {
+    this.userReviewMarkService.getUserReviewMark(id, (success) => {
+      this.userReviewMark = <UserReviewMark>success;
+    }
+    );
+  }
+  testPost() {
+    this.userReviewMarkService.createUserReviewMark(this.userReviewMark, (success) => {
+      this.userReviewMark = <UserReviewMark>success;
+      this.testAll();
+    });
+  }
+  testPut = (id: number) => {
+    if (!confirm('This Mark(id =' + id + ') will be updated')) {
+      return;
+    }
+    this.userReviewMarkService.updateUserReviewMark(id, this.userReviewMark, (success) => {
+      this.userReviewMark = <UserReviewMark>success;
+      this.testAll();
+    });
+  }
+ 
+  testDelete = (id: number) => {
+    if (!confirm('This Mark(id =' + id + ') will be removed')) {
+      return;
+    }
+    this.userReviewMarkService.deleteUserReviewMark(id, (success) => {
+      this.testAll();
+    });
+  }
+  testAll() {
+    this.userReviewMarkService.getAll( (success) => {
+      this.userReviewMarkList = <UserReviewMark[]>success;
+    });
+  }
 }
